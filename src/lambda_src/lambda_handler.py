56dbed96233da55f295f29258dc951370e4f64af
jpang32 <jpangallozzi@gmail.com>
2023-10-25 21:27:54 -0400

    Lambda handler with GPT-3.5 request and integration test

---
+++ src/lambda_src/lambda_handler.py
@@ -0,0 +1,80 @@
+import logging
+import json
+import os
+import requests
+
+from typing import Dict, List
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+OPEN_AI_TOKEN = os.getenv("OPEN_AI_TOKEN")
+OPEN_AI_MODEL = os.getenv("OPEN_AI_MODEL")
+
+def lambda_handler(event, context):
+    # get header method and check if POST
+    # If not, return error
+    http_method = event.get("http_method", "POST")
+    if http_method.upper() != "POST":
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "isBase64Encoded": False,
+            "body": json.dumps({"message":"Invalid HTTP method used"})
+        }
+
+    # Call function get response from GPT-4
+    initial_stories = get_openai_model_response()
+
+    return {"message": initial_stories}
+
+
+def get_openai_model_response(image_captions: List[Dict[str, str]]):
+
+    url = "https://api.openai.com/v1/chat/completions"
+
+    system_content = "Given a list of dictionaries of photo captions from a vlog in the format " \
+                 " of this example: [{'image_path': 'birthday/BpsSOqpog98/" \
+                 "BpsSOqpog98-0190.jpg', 'image_caption': 'a child blowing out candles" \
+                 " at a birthday party'}, {...}, ...]" \
+                 ", create a vivid story " \
+                 "that incorporates the key elements from each " \
+                 "photo. Turn the photo descriptions into a fun and " \
+                 "engaging story. Your response should be a dictionary following the " \
+                 "format of this example: [{'image_path': 'birthday/BpsSOqpog98/" \
+                 "BpsSOqpog98-0190.jpg', 'image_caption': 'a child blowing out candles" \
+                 " at a birthday party', 'story': 'the story you generated'}, {...}, ...]"
+
+    user_content = json.dumps(image_captions)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPEN_AI_TOKEN}"
+    }
+
+    body = {
+        "model": OPEN_AI_MODEL,
+        "messages": [
+            {
+                "role": "system",
+                "content": system_content
+            },
+            {
+                "role": "user",
+                "content": user_content
+            }
+        ]
+    }
+
+    open_ai_response = requests.post(
+        url=url,
+        json=body,
+        headers=headers
+    ).json()
+
+    model_response = open_ai_response["choices"][0]["message"]["content"]
+
+
+
---
+++ test/integration/test_lambda_handler.py
@@ -0,0 +1,33 @@
+import json
+import pytest
+from src.lambda_src.lambda_handler import get_openai_model_response
+
+@pytest.mark.parametrize(
+    "image_captions",
+    [
+        [
+            {
+                "image_path": "test_path/image1.png",
+                "image_caption": "a child pulls on a tablecloth"
+            },
+            {
+                "image_path": "other_test_path/image2.png",
+                "image_caption": "a child starts to pull a tablecloth off of the table"
+            },
+            {
+                "image_path": "other_other_test_path/image2.png",
+                "image_caption": "a child covered in food in front of a table with food on the floor"
+            }
+        ]
+    ]
+)
+def test_get_openai_model_response(image_captions):
+
+    result = get_openai_model_response(image_captions)
+    result_json = json.loads(result)
+
+    for image_caption in image_captions:
+        assert result_json["image_path"] == image_caption["image_path"]
+        assert result_json["image_caption"] == image_caption["image_caption"]
+        assert "story" in result_json
+
