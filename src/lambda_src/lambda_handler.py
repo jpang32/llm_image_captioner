1a5f23f2f344a53f5de2813fe0b731f116db51c0
jpang32 <jpangallozzi@gmail.com>
2023-10-28 19:43:11 -0400

    Reduce code by using custom Response object

--- src/lambda_src/lambda_handler.py
+++ src/lambda_src/lambda_handler.py
@@ -1,42 +1,39 @@
 import json
 import logging
+from typing import Any
+
 from src.lambda_src.utils import get_openai_model_response, validate_request_body
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+class Response:
+
+    def __new__(cls, status_code: int, body: Any):
+        return {
+            "statusCode": status_code,
+            "headers": {"Content-Type": "application/json"},
+            "body": body
+        }
+
+
 def lambda_handler(event, context):
     http_method = event.get("http_method", "POST")
+    request_body = json.loads(event["body"])
+
     if http_method.upper() != "POST":
         logger.error(f"Invalid HTTP method used ({http_method})")
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": "Invalid HTTP method used"
-        }
+        return Response(status_code=400, body="Invalid HTTP method used")
 
-    request_body = json.loads(event["body"])
     is_valid_request_body = validate_request_body(request_body)
+
     if not is_valid_request_body:
         logger.error(f"Invalid request body ({request_body})")
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": "Invalid request body. Must be in form [{image_path:'', image_captions:''}, {...}, ...]"
-        }
+        return Response(
+            status_code=400,
+            body="Invalid request body. Must be in form [{image_path:'', image_captions:''}, {...}, ...]"
+        )
 
-    # Call function get response from GPT-4
-    initial_stories = get_openai_model_response(image_captions=request_body)
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": initial_stories
-    }
+    stories = get_openai_model_response(image_captions=request_body)
+    return Response(status_code=200, body=stories)
